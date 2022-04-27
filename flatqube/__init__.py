54c2c34a904ea531a1ade2c0371d8ae98407a118
Evgeny Prilepin <esp.home@gmail.com>
2022-04-27 17:25:20 +0300

    Add main classes to __init__/__all__ to direct import

--- flatqube/__init__.py
+++ flatqube/__init__.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+
+from .client import FlatQubeClient, FlatQubeClientError, CurrencySortOptions, CurrencySortOrders
+from .models import CurrencyInfo, FarmingPoolInfo
+
+from .version import __version__  # noqa
+
+
+__all__ = [
+    'FlatQubeClient',
+    'FlatQubeClientError',
+    'CurrencySortOptions',
+    'CurrencySortOrders',
+    'CurrencyInfo',
+    'FarmingPoolInfo',
+]
