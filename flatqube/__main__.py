d918b740c5a978b9196b1ab9702e3eaaf9bee433
Evgeny Prilepin <esp.home@gmail.com>
2022-05-01 11:57:22 +0300

    Add __main__ module to run the module as CLI

---
+++ flatqube/__main__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Use the module as CLI tool from Python interpreter directly::
+
+    python -m flatqube --help
+
+"""
+
+from .cli import cli
+
+
+if __name__ == '__main__':
+    cli()
